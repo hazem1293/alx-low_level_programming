3ad3fa06982ead4c1ec323008070bfbdf7085935
hazem Khaled <hazem.khaled1293@gmail.com>
2023-10-17 00:09:28 +0300

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - checks for uppercase character
+ * @s: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	int i = 0;
+
+	for (i == 0; *s != "/0"; i++)
+	{
+		count++;
+	}
+
+	return (count);
+}
